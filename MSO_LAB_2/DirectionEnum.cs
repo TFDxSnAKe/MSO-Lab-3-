b4b6ebd343a02a98051e7bbd9459c6e2f7c824a0
TFDxSnAKe <jeremy678smith@hotmail.com>
2025-10-12 17:20:32 +0200

    Small start

---
+++ MSO_LAB_2/DirectionEnum.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSO_LAB_2
+{ 
+    public enum Direction { North, East, South, West}    
+}
---
+++ MSO_LAB_2/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSO_LAB_2
+{
+    internal class Player
+    {
+        Point position = new(); // default is (0,0)
+        Direction direction = Direction.East;
+
+        Player()
+        {
+            //
+        }
+    }
+}
