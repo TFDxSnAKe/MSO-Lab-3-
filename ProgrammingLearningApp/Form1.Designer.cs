19dada4d2b42f6c6b71776b408c4c35eeb429c03
TFDxSnAKe <jeremy678smith@hotmail.com>
2025-10-28 22:34:05 +0100

    Actual functionality for opening .txt files and saving implemented

--- ProgrammingLearningApp/Form1.Designer.cs
+++ ProgrammingLearningApp/Form1.Designer.cs
@@ -30,32 +30,74 @@
         {
             OutputBox = new TextBox();
             ExecuteProgramButton = new Button();
+            EditorWindow = new TextBox();
+            SaveButton = new Button();
+            OpenProgram = new Button();
             SuspendLayout();
             // 
             // OutputBox
             // 
-            OutputBox.Location = new Point(457, 319);
+            OutputBox.BackColor = SystemColors.Window;
+            OutputBox.Font = new Font("Cascadia Code", 15.75F);
+            OutputBox.Location = new Point(752, 465);
             OutputBox.Multiline = true;
             OutputBox.Name = "OutputBox";
-            OutputBox.Size = new Size(304, 105);
+            OutputBox.ReadOnly = true;
+            OutputBox.ScrollBars = ScrollBars.Both;
+            OutputBox.Size = new Size(304, 193);
             OutputBox.TabIndex = 0;
             OutputBox.TextChanged += OutputBox_TextChanged;
             // 
             // ExecuteProgramButton
             // 
-            ExecuteProgramButton.Location = new Point(75, 319);
+            ExecuteProgramButton.Location = new Point(12, 102);
             ExecuteProgramButton.Name = "ExecuteProgramButton";
-            ExecuteProgramButton.Size = new Size(236, 105);
+            ExecuteProgramButton.Size = new Size(136, 38);
             ExecuteProgramButton.TabIndex = 1;
             ExecuteProgramButton.Text = "Execute Program (Test)";
             ExecuteProgramButton.UseVisualStyleBackColor = true;
             ExecuteProgramButton.Click += button1_Click;
             // 
+            // EditorWindow
+            // 
+            EditorWindow.Font = new Font("Cascadia Code", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            EditorWindow.Location = new Point(154, 12);
+            EditorWindow.Multiline = true;
+            EditorWindow.Name = "EditorWindow";
+            EditorWindow.ScrollBars = ScrollBars.Both;
+            EditorWindow.Size = new Size(592, 646);
+            EditorWindow.TabIndex = 2;
+            EditorWindow.TextChanged += EditorWindow_TextChanged;
+            // 
+            // SaveButton
+            // 
+            SaveButton.Location = new Point(12, 58);
+            SaveButton.Name = "SaveButton";
+            SaveButton.Size = new Size(136, 38);
+            SaveButton.TabIndex = 3;
+            SaveButton.Text = "Save Program";
+            SaveButton.UseVisualStyleBackColor = true;
+            SaveButton.Click += SaveButton_Click;
+            // 
+            // OpenProgram
+            // 
+            OpenProgram.Location = new Point(12, 14);
+            OpenProgram.Name = "OpenProgram";
+            OpenProgram.Size = new Size(136, 38);
+            OpenProgram.TabIndex = 4;
+            OpenProgram.Text = "Open Program";
+            OpenProgram.UseVisualStyleBackColor = true;
+            OpenProgram.Click += OpenProgram_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(1068, 670);
+            Controls.Add(OpenProgram);
+            Controls.Add(SaveButton);
+            Controls.Add(EditorWindow);
             Controls.Add(ExecuteProgramButton);
             Controls.Add(OutputBox);
             Name = "Form1";
@@ -69,5 +111,8 @@
 
         private TextBox OutputBox;
         private Button ExecuteProgramButton;
+        private TextBox EditorWindow;
+        private Button SaveButton;
+        private Button OpenProgram;
     }
 }
--- ProgrammingLearningApp/Form1.cs
+++ ProgrammingLearningApp/Form1.cs
@@ -9,7 +9,7 @@ namespace ProgrammingLearningApp
         public string FilePath;
         public Player Player;
         public MSO_LAB_3.Program Program;
-
+        //StreamWriter ProgramWriter;
         public Form1()
         {
             InitializeComponent();
@@ -32,8 +32,45 @@ namespace ProgrammingLearningApp
 
         private void OutputBox_TextChanged(object sender, EventArgs e)
         {
-            //
+            // kinda useless
         }
+
+
+        private void EditorWindow_TextChanged(object sender, EventArgs e)
+        {
+            /*
+            var editorLines = EditorWindow.Lines;
+            ProgramWriter = new StreamWriter(PathHelper("Programs\\Test"));
+
+            foreach (var line in editorLines)
+            {
+                ProgramWriter.WriteLine(line);
+            }*/
+        }
+        private void OpenProgram_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK && openFileDialog.FileName.Contains(".txt"))
+            {
+                string contents = File.ReadAllText(openFileDialog.FileName);
+                EditorWindow.Text = contents;
+            }
+            else
+            {
+                MessageBox.Show("Invalid file format (Must be a .txt file)");
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string programName = saveFileDialog.FileName + ".txt";
+                File.WriteAllText(programName, EditorWindow.Text);
+            }
+        }
+
 
         /// <summary>
         /// A little path helper which reuses the old one
@@ -42,7 +79,9 @@ namespace ProgrammingLearningApp
         /// <returns></returns>
         private string PathHelper(string name)
         {
-            return MSO_LAB_3.App.GetPath(name);
+            return App.GetPath(name);
         }
+
+
     }
 }
