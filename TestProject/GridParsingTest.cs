443ea24ffd980e052bf1c3eb218a0e1a8100576a
TFDxSnAKe <jeremy678smith@hotmail.com>
2025-11-01 23:10:05 +0100

    Added tests for checking if grid parsing is done correctl (it is)

--- TestProject/GridParsingTest.cs
+++ TestProject/GridParsingTest.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace TestProject
 {
+
     public class GridParsingTest
     {
         [Fact]
-        public void Test()
+        public void ExampleGridTest()
         {
             string tempFile = Path.GetTempFileName();
             File.WriteAllLines(tempFile, new[]
@@ -20,6 +21,58 @@ namespace TestProject
                     "++o++",
                     "++oox"
                 });
+
+
+            var gridReader = new MSO_LAB_3.GridFileRead(tempFile);
+
+            Assert.Equal(5, gridReader.Grid.GetLength(0));
+            Assert.Equal(5, gridReader.Grid.GetLength(1));
+
+            Assert.Equal('o', gridReader.Grid[0, 0]);
+            Assert.Equal('o', gridReader.Grid[1,0]);
+            Assert.Equal('+', gridReader.Grid[0,2]);
+            Assert.Equal('+', gridReader.Grid[0,1]);
+
+            Assert.Equal('x', gridReader.Grid[4, 4]);
+
+        }
+
+        [Fact]
+        public void PossibleRealGridTest()
+        {
+            string tempFile = Path.GetTempFileName();
+            File.WriteAllLines(tempFile, new[]
+                {  
+                   // Our random mock 10x10 grid
+                   //
+                   // x ->
+                   //0123456789         y
+                    "oooooooooo", // 0  |
+                    "o++++++++o", // 1  v
+                    "o+++oo+++o", // 2
+                    "o+++oo+++o", // 3
+                    "+++++++++o", // 4
+                    "o++++++++o", // 5
+                    "o+++oo+++o", // 6
+                    "o+++oo+++o", // 7
+                    "o++++++++o", // 8
+                    "xooooooooo"  // 9
+                });
+
+
+            var gridReader = new MSO_LAB_3.GridFileRead(tempFile);
+
+            Assert.Equal(10, gridReader.Grid.GetLength(0));
+            Assert.Equal(10, gridReader.Grid.GetLength(1));
+
+            Assert.Equal('o', gridReader.Grid[0, 0]);
+            Assert.Equal('o', gridReader.Grid[1, 0]);
+            Assert.Equal('o', gridReader.Grid[0, 2]);
+            Assert.Equal('+', gridReader.Grid[0, 4]);
+            Assert.Equal('+', gridReader.Grid[8, 4]);
+            Assert.Equal('+', gridReader.Grid[1, 8]);
+
+            Assert.Equal('x', gridReader.Grid[0, 9]);
 
         }
     }
