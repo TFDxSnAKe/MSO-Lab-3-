a4518b7860433ccf7ddf02f5ead2e64f032ceb87
ItsDDaam <Daambanaan@gmail.com>
2025-11-04 00:36:31 +0100

    gridfilereader geupdate, zodat de link met Grid class goed werkt.
    De test is nu niet meer juist, omdat het de verkeerde dingen test. Volgende stap is test maken

--- MSO_LAB_2/GridFileRead.cs
+++ MSO_LAB_2/GridFileRead.cs
@@ -8,31 +8,24 @@ namespace MSO_LAB_3
 {
     public class GridFileRead
     {
-        public char[,] Grid;
         
-        public GridFileRead(string filePath)
+        
+        public Grid LoadGridFile(string filePath)
         {
             var lines = File.ReadAllLines(filePath);
-            Grid = new char[lines[0].Length, lines.Length];
-            ExtractGrid(filePath);
-        }
 
-        public void ExtractGrid(string path)
-        {
-            StreamReader reader = new StreamReader(path);
-            var line = reader.ReadLine();
-            int y = 0;
-            while (line != null)
+            //Grid = new char[lines[0].Length, lines.Length];
+            char[,] cells = new char[lines[0].Length, lines.Length];
+            
+            for (int y = 0; y < lines.Length; y++)
             {
-                int x = 0;
-                foreach (char c in line)
+                for (int x = 0; x < lines[y].Length; x++)
                 {
-                    Grid[x, y] = c;
-                    x++;
+                    cells[x, y]= lines[y][x];
                 }
-                y++;
-                line = reader.ReadLine();
             }
+
+            return new Grid(cells);
         }
     }
 }
