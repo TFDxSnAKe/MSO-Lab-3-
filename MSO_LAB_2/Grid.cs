1784c4362e5dd424a4e7abd9780c35a6fb43cd65
ItsDDaam <Daambanaan@gmail.com>
2025-11-03 18:41:18 +0100

    added basic grid class
    De grid heeft nu zijn eigen class, en eigen methods die de grid kunnen bouwen, en kunnen bepalen of een vector in de grid is zodat ik de speler binnen de grid kan houden

---
+++ MSO_LAB_2/Grid.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSO_LAB_3
+{
+    public class Grid
+    {
+
+        public char[,] Cells { get; private set; }
+
+        public int Width => Cells.GetLength(0);
+        public int Height => Cells.GetLength(1);
+
+        public Grid(char[,] cells)
+        {
+            Cells = cells;
+        }
+        
+        public bool Contains(Vector2 position)
+        {
+            return position.X >= 0 && position.X < Width && position.Y >= 0 && position.Y < Height;
+        }
+
+        public char GetCell(Vector2 position)
+        {
+            return Cells[(int)position.X, (int)position.Y];
+        }
+
+        public void SetCell(Vector2 position, char value)
+        {
+            Cells[(int)position.X, (int)position.Y] = value;
+        }
+    }
+}
