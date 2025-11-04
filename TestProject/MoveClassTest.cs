d34593a1549a8cd06fd8c34fd2542cf4ee3fd21f
ItsDDaam <Daambanaan@gmail.com>
2025-11-04 02:36:10 +0100

    Testing Move class

---
+++ TestProject/MoveClassTest.cs
@@ -0,0 +1,41 @@
+﻿using MSO_LAB_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class MoveClassTest
+    {
+
+        [Fact]
+        public void MovePlayerToPostitionInsideGridCorrectly()
+        {
+            char[, ] cells = new char[6, 6];
+            Grid grid = new Grid(cells);
+            Player player = new Player {position = new Vector2(3,3)};
+            Move move = new Move(grid, 1);
+
+            move.Execute(player);
+
+            Assert.Equal(new Vector2(3, 2), player.position);
+        }
+
+        [Fact]
+        public void MovePlayerToPostitionOutsideGridCorrectly()
+        {
+            char[,] cells = new char[6, 6];
+            Grid grid = new Grid(cells);
+            Player player = new Player { position = new Vector2(3, 3) };
+            Move move = new Move(grid, 6);
+
+            move.Execute(player);
+
+            Assert.Equal(new Vector2(3, 3), player.position);
+        }
+
+    }
+}
