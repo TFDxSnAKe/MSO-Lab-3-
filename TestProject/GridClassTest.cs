289cca9f6d5f93f4ad33996d4cb1235e98ddf634
ItsDDaam <Daambanaan@gmail.com>
2025-11-04 01:13:26 +0100

    Last two tests also complete
    yuh

--- TestProject/GridClassTest.cs
+++ TestProject/GridClassTest.cs
@@ -11,25 +11,44 @@ namespace TestProject
     public class GridClassTest
     {
         [Fact]
-        public void TestContains() //Given a position inside the grid and a pos outside
+        public void TestContains_returnsCorrectBool() //Given a position inside the grid and a pos outside
         {
-            //setup grid
             var cells = new char[10, 10];
             var grid = new Grid(cells);
 
             bool isInside = grid.Contains(new Vector2(3, 2));
             bool isOutside = grid.Contains(new Vector2(20, 2));
 
-            
             Assert.True(isInside);
             Assert.False(isOutside);
 
 
         }
 
+        [Fact]
+        public void GetCell_ReturnsCorrectCharacter()
+        {
+            var cells = new char[3, 3];
+            cells[1, 2] = 'X';
+            var grid = new Grid(cells);
 
 
+            var checkCell = grid.GetCell(new Vector2(1, 2));
 
+
+            Assert.Equal('X', checkCell);
+        }
+
+        [Fact]
+        public void SetCell_ChangesValueAtPositionCorectly() 
+        {
+            var grid = new Grid(new char[3, 3]);
+            var pos = new Vector2(1, 1);
+
+            grid.SetCell(pos, 'A');
+
+            Assert.Equal('A', grid.GetCell(pos));
+        }
 
 
 
